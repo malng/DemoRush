24827d9b80eb12573f4e18ee605c0b2c6a242333
mal818 <mal818@126.com>
2019-05-16 10:28:56 +0800

    a little change for connecter/J 8

--- jdbc_demo/src/main/java/design/maliang/jdbc/JDBCBase7Step.java
+++ jdbc_demo/src/main/java/design/maliang/jdbc/JDBCBase7Step.java
@@ -19,24 +19,25 @@ public class JDBCBase7Step {
         try {
             //2.注册驱动
             //DriverManager.registerDriver(new Driver());
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
             //3.获取连接对象
             //Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/web03", "root", "123456");
             //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/web03", "root", "123456");
-            conn = DriverManager.getConnection("jdbc:mysql:///web03", "root", "123456");
+//            conn = DriverManager.getConnection("jdbc:mysql:///db_demorush", "root", "123456");
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/db_demorush?serverTimezone=GMT%2B8","root","123456");
 
             //4.获取可以操作SQL语句的对象
             stat = conn.createStatement();
 
             //5.执行SQL语句，获取结果集
-            String sql = "select * from t_users;";
+            String sql = "select * from users;";
             rs = stat.executeQuery(sql);
 
             //6.操作结果集
             while (rs.next()) {
                 //可以用字符串查找
-                int uid = rs.getInt("uid");
+                int uid = rs.getInt("id");
                 //也可以用index查找
                 String username = rs.getString(2);
                 String password = rs.getString("password");
